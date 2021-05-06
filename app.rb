64583004369d5081f2ab7fae97380b6481af3289
arozputnii <a.rpzutnii@gmail.com>
2021-05-06 17:55:56 +0300

    Flex element

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
 class App < Sinatra::Base
-  get '/' do
-    erb :index
-  end
+  get('/') { erb :'flex-blocks/index' }
+
+  get('/flex-elements') { erb :'flex-elements/index' }
 end

08f124d9e2f8cf3acf783f344d2f546042e51519
arozputnii <a.rpzutnii@gmail.com>
2021-05-07 16:29:00 +0300

    practice task

--- app.rb
+++ app.rb
@@ -4,4 +4,6 @@ class App < Sinatra::Base
   get('/') { erb :'flex-blocks/index' }
 
   get('/flex-elements') { erb :'flex-elements/index' }
+
+  get('/task'){ erb :"pages/task", layout: :practice }
 end
